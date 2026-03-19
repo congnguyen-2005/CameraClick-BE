e49d3e8c881fcc1ea0420f8d8e2f8507b60fa8a2
Cong1235 <ccon56852@gmail.com>
2026-03-20 02:42:15 +0700

    fix cors + config env

--- config/cors.php
+++ config/cors.php
@@ -8,7 +8,9 @@ return [
 
     // SỬA DÒNG NÀY: Thay localhost bằng dấu '*' 
     // Dấu '*' nghĩa là cho phép mọi nguồn (bao gồm cả Vercel) truy cập
-    'allowed_origins' => ['*'], 
+'allowed_origins' => [
+    'https://camera-click-fe-3q2k-onrpjoc81-congnguyen-2005s-projects.vercel.app'
+],
 
     'allowed_origins_patterns' => [],
 
