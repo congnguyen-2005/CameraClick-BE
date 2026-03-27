4685b2ffe7a720034d189bf03ce9b613be77f72a
Cong1235 <ccon56852@gmail.com>
2026-03-28 02:06:46 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable, HasFactory;
+
+    // 👇 CHỐT CHẶN QUAN TRỌNG: Ép Laravel dùng đúng bảng ntc_users
+    protected $table = 'ntc_users';
+
     public $timestamps = true; 
 
     protected $fillable = [
@@ -17,13 +21,11 @@ class User extends Authenticatable
         'avatar', 'created_at', 'created_by', 'updated_at', 'updated_by', 'status'
     ];
 
-
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
- 
     protected $casts = [
         'password' => 'hashed',
         'status' => 'integer',
